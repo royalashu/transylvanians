8bef3d01ec3043febe435a3191937457c7d71040
Ashish kumar <71541701+royalashu@users.noreply.github.com>
2024-04-23 17:17:00 +0530

    Add files via upload

---
+++ lode/script.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+// gsap.to('#loder', {
+//             height:'100vh',
+//             duration:2,
+//         })
+gsap.to('#loder', {
+    display:'none',
+delay:3,
+duration:1,
+scale:100,
+    mixBlendMode:'darken',
+        })
