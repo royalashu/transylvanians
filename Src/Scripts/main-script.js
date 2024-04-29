355cf4648fd9411ef3819d91a5d67b1dc32e1e29
Ashish kumar <71541701+royalashu@users.noreply.github.com>
2024-04-29 21:26:56 +0530

    Update main-script.js

--- Src/Scripts/main-script.js
+++ Src/Scripts/main-script.js
@@ -218,3 +218,59 @@ window.addEventListener("DOMContentLoaded", () => {
 
   create();
 });
+
+
+// mouse
+
+var cursor = $(".cursor"),
+    follower = $(".cursor-follower");
+
+var posX = 0,
+    posY = 0;
+
+var mouseX = 0,
+    mouseY = 0;
+
+TweenMax.to({}, 0.016, {
+  repeat: -1,
+  onRepeat: function() {
+    posX += (mouseX - posX) / 9;
+    posY += (mouseY - posY) / 9;
+    
+    TweenMax.set(follower, {
+        css: {    
+        left: posX - 12,
+        top: posY - 12
+        }
+    });
+    
+    TweenMax.set(cursor, {
+        css: {    
+        left: mouseX,
+        top: mouseY
+        }
+    });
+  }
+});
+
+document.querySelector('body').addEventListener("mousemove", function(e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+});
+
+$(".card").on("mouseenter", function() {
+    cursor.addClass("active");
+    follower.addClass("active");
+});
+$(".card").on("mouseleave", function() {
+    cursor.removeClass("active");
+    follower.removeClass("active");
+});
+$("a").on("mouseenter", function() {
+    cursor.addClass("active");
+    follower.addClass("active");
+});
+$("a").on("mouseleave", function() {
+    cursor.removeClass("active");
+    follower.removeClass("active");
+});
